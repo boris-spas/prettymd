f33d93c6efc58083bce5439d810911ef901fb1e4
Pascal Maissen <pascal.maissen@reconline.com>
2018-11-01 15:10:59 +0100

    Simple class for a json object

--- src/main/java/com/pascal/prettymd/JsonObject.java
+++ src/main/java/com/pascal/prettymd/JsonObject.java
@@ -2,17 +2,14 @@ package com.pascal.prettymd;
 
 public class JsonObject {
 	
-	public String type;
 	public String text;
 	public String options;
 	
-	public JsonObject(String type, String text) {
-		this.type = type;
+	public JsonObject(String text) {
 		this.text = text;
 	}
 	
-	public JsonObject(String type, String text, String options) {
-		this.type = type;
+	public JsonObject(String text, String options) {
 		this.text = text;
 		this.options = options;
 	}
